73c9544330a1b885f6f774fe3de6bfa5e4ee9bd4
Carlos Sanchez Rodriguez (cafijo) <csanchez@thehotelsnetwork.com>
2023-04-09 15:38:17 +0200

    feat(v1):  add version

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/sh -l
-/main --openai_api_key "$1" --github_token "$2" --github_pr_id $3 --github_user "$4" --github_repo "$5"
+/main --openai_api_key "$1" --github_token "$2" --github_pr_id $3
